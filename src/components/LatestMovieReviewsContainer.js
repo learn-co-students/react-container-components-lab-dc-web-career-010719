b7c1b6497f5a44bd243ff54659ef3366c21a7a8b
dcjones1 <dcejjones@gmail.com>
2019-03-14 18:20:56 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,52 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+
+
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchReviews()
+  }
+
+  fetchReviews() {
+    let reviews = [...this.state.reviews]
+    fetch('//api.nytimes.com/svc/movies/v2/reviews/all/json?api-key=MMj69ol8Y0b9VVFuAbjFyvDhbtp6Y10o')
+    .then(res => res.json())
+    .then(data => {
+      let results = data.results
+      results.forEach((review) => {
+        reviews.push(review)
+      })
+      this.setState({reviews})
+    })
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h1>Latest Movie Reviews</h1>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+// This is how I would do it. Map movies in the reviews then pass one at a time down.
+{/* <div className="latest-movie-reviews">
+  <h1>Latest Movie Reviews</h1>
+  {this.state.reviews.map((review) => {
+    return <MovieReviews reviews={review} key={review.display_title}/>
+  })}
+</div> */}
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,45 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export const MovieReviews = (props) => {
+  return (
+    <div className="review-list">
+      {props.reviews.map((review) => {
+        let image_url
+        if (!review.multimedia) {
+          image_url = "https://www.shareicon.net/data/256x256/2016/07/08/117367_logo_512x512.png"
+        } else {
+          image_url = review.multimedia.src
+        }
+
+        return (
+          <div className="review" style={{float: 'left',width: '50%'}}>
+            <h2>{review.display_title}</h2>
+            <img src={image_url} alt="movie poster" />
+            <p>{review.summary_short}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+// This is how I would do it. Map movies in the reviews then pass one at a time down.
+// let {display_title, summary_short} = props.reviews
+// let image_url
+// if (!props.reviews.multimedia) {
+//   image_url = "https://www.shareicon.net/data/256x256/2016/07/08/117367_logo_512x512.png"
+// } else {
+//   image_url = props.reviews.multimedia.src
+// }
+//
+// return (
+//   <div className="review-list">
+//     <div className="review" style={{float: 'left',width: '50%'}}>
+//       <h2>{display_title}</h2>
+//       <img src={image_url} alt="movie poster" />
+//       <p>{summary_short}</p>
+//     </div>
+//   </div>
+// )
+
+export default MovieReviews
